8a62f3700e7893266e51486b8595b704aa206ace
Ben Aaron <ben@theaarons.com>
2021-01-24 10:46:15 -0800

    return nan instead of error

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"math"
 	"syscall/js"
 )
 
@@ -15,7 +15,9 @@ func main() {
 func getNthFibonacci(n int) int {
 	// I know a recursive solution requires polynomial time, that's why I'm
 	// using it, how far can WASM be pushed?
-	if n <= 1 {
+	if n < 0 {
+		math.NaN()
+	} else if n <= 1 {
 		return n
 	}
 	return getNthFibonacci(n-1) + getNthFibonacci(n-2)
@@ -26,14 +28,10 @@ func jsonWrapper() js.Func {
 		if len(args) != 1 {
 			return "Invalid no of arguments passed"
 		}
-		inputJSON := args[0].String()
-		fmt.Printf("input %s\n", inputJSON)
-		pretty, err := prettyJSON(inputJSON)
-		if err != nil {
-			fmt.Printf("unable to convert to json %s\n", err)
-			return err.Error()
-		}
-		return pretty
+		inputN := args[0].String().atoi()
+		fmt.Printf("input %d\n", inputN)
+		fibN := getNthFibonacci(inputN)
+		return fibN
 	})
 	return jsonFunc
 }
